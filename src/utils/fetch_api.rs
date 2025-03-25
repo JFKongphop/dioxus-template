fa7bd28b578ab820ab8e931564ec059ca2424f25
JFKongphop <kongphopleo@gmail.com>
2025-03-25 14:35:01 +0700

    update fetch api function

--- src/utils/fetch_api.rs
+++ src/utils/fetch_api.rs
@@ -1,17 +1,26 @@
 use crate::types::running_response_types::{DateDistance, DateDistanceResponse};
-use reqwest::Client;
+use reqwest::{Client, Error};
+use serde::de::DeserializeOwned;
 
-pub async fn get_month_daily_distance() -> Result<Vec<DateDistance>, reqwest::Error> {
-  let url = "https://running-api-1007595656483.asia-east2.run.app/api/v1/month-day-distance?year_month=2568-02";
-
-  let response = Client::new()
+async fn fetch_running_data<T>(path: &str) -> Result<T, Error> 
+where 
+  T: DeserializeOwned
+{
+  let url = format!("https://running-api-1007595656483.asia-east2.run.app/api/v1{}", path);
+  
+  Ok(Client::new()
     .get(url)
     .send()
     .await?
     .error_for_status()?
-    .json::<DateDistanceResponse>()
-    .await?;
+    .json::<T>()    
+    .await?
+  )
+}
 
+pub async fn get_month_daily_distance() -> Result<Vec<DateDistance>, Error> {
+  let path = "/month-day-distance?year_month=2568-02";
+  let response: DateDistanceResponse = fetch_running_data(path).await?;
   let date_distance = response.date_distance;
 
   Ok(date_distance)
